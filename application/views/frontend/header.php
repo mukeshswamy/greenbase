3d0ba5f267b73cb324da9cd4c7afe5bcbb92c4f8
Mukesh swamy <59552894+mukeshswamy@users.noreply.github.com>
2020-08-20 15:40:28 +0530

    home-page-day-1

--- application/views/frontend/header.php
+++ application/views/frontend/header.php
@@ -4,6 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href=<?php echo base_url('assets/css/bootstrap.min.css');?>>
+    <link rel="stylesheet" href=<?php echo base_url('assets/css/style.css')?>>
+    <script src=<?php echo base_url('assets/js/jquery.min.js');?>></script>
     <title>GreenBase | Home</title>
 </head>
 <body>
+<header class="main-header">
+    <div class="container-fluid container-header">
+        <div class="row header-row">
+            <div class="col-md-2 paddingnull">
+                <img src=<?php echo base_url('assets/images/logo.svg');?> alt="logo" class="logo">
+            </div>
+            <div class="col-md-10">
+                <ul class="main-nav-ul">
+                    <li class="active">ABOUT US</li>
+                    <li>WHY US</li>
+                    <li>OUR LOCATION</li>
+                    <li>OUR CLIENTS</li>
+                    <li>NEWS</li>
+                    <li>GALLERY</li>
+                    <li>BLOGS</li>
+                    <li>CONTACT US</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
--- application/views/frontend/home.php
+++ application/views/frontend/home.php
@@ -1,3 +1,142 @@
 <?php include('header.php')?>
+<!-- SECTION 1 -->
+<section>
+    <div id="carouselControlheader" class="carousel slide carousel-container" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src=<?php echo base_url('assets/images/banner1.jpg');?> alt="First slide" />
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src=<?php echo base_url('assets/images/banner1.jpg');?> alt="Second slide" />
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src=<?php echo base_url('assets/images/banner1.jpg');?> alt="Third slide" />
+            </div>
+        </div>
+    </div>
+    <div class="carousel-controler-container">
+        <a href="#carouselControlheader" role="button" data-slide="next">
+            <img src=<?php echo base_url('assets/images/next.svg')?> alt="next" />
+        </a>
+        </br>
+        <a href="#carouselControlheader" role="button" data-slide="prev">
+            <img src=<?php echo base_url('assets/images/prev.svg')?> alt="next" />
+        </a>
+    </div>
+    <div class="container-fluid content-over-header">
+        <div class="row">
+            <div class="col-md-12 paddingnull">
+                <div class="content-div">
+                    “Hiranandani Group’s intrinsic strength
+                </div></br>
+                <div class="content-div">
+                    in infrastructure and township development gives
+                </div></br>
+                <div class="content-div">
+                    Greenbase a natural advantage over competition”
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Section 2 -->
+<section>
+    <div class="container-fluid capabilitie-container">
+        <div class="row">
+            <div class="col-md-4 content2-div">
+                <div class="content-2">
+                    OUR
+                </div></br>
+                <div class="content-2">
+                    CAPABILITIES
+                </div>
+                <div class="content-2-1">
+                    Our commercial expertise paired with cutting edge infrastructure and
+                    execution capabilities enables us to provide cost-effective solutions
+                    for all industrial and warehousing spaces.
+                </div>
+                <ul class="content-2-ul">
+                    <li>BUILT TO SUIT INDUSTRIAL FACILITIES</li>
+                    <li>PLUG AND PLAY FACILITIES FOR LIGHT INDUSTRIAL STORAGE</li>
+                    <li>COLD STORAGE</li>
+                    <li>WAREHOUSE DEVELOPMENTS</li>
+                </ul>
+            </div>
+            <div class="col-md-8 carousel-container3">
+                <img src=<?php echo base_url('assets/images/banner2.jpg');?> alt="banner2" class="banner2">
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Section 3 -->
+<section>
+    <div class="container-fluid capabilitie-container2">
+        <div class="row">
+            <div class="col-md-4 capabilitie-content-div">
+                <ul class="capabilitie-content-2-ul">
+                    <li>BUILT TO SUIT INDUSTRIAL FACILITIES</li>
+                    <li>PLUG AND PLAY FACILITIES FOR LIGHT INDUSTRIAL STORAGE</li>
+                    <li>COLD STORAGE</li>
+                    <li>WAREHOUSE DEVELOPMENTS</li>
+                </ul>
+                <div class="capabilitie-content-2-1">
+                    <h4>BUILT TO SUIT INDUSTRIAL FACILITIES</h4>
+                    We understand every customer is unique. From conceptualizing design</br>
+                    till delivery of the facility and beyond we take into consideration every</br>
+                    minute detail from the client.</br>
+                    An additional advantage of build-to-suit solutions is the free choice of</br>
+                    location, playing to our strengths. A built-to-suit structure gives clients </br>
+                    the flexibility in terms of the size of industrial area to be developed.
+                </div>
+                <div class="capabilitie-content-2">
+                    OUR
+                </div></br>
+                <div class="capabilitie-content-2 length">
+                    CAPABILITIES
+                </div>
+            </div>
+            <div id="carouselControl" class="col-md-8 carousel slide carousel-container3" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src=<?php echo base_url('assets/images/banner1.jpg');?>
+                            alt="First slide" />
+                        <div class="text-inner-capabilities">
+                            BUILT TO SUIT
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src=<?php echo base_url('assets/images/banner1.jpg');?>
+                            alt="Second slide" />
+                        <div class="text-inner-capabilities">
+                            PLUG & PLAY
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src=<?php echo base_url('assets/images/banner1.jpg');?>
+                            alt="Third slide" />
+                        <div class="text-inner-capabilities">
+                            COLD STORAGE
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src=<?php echo base_url('assets/images/banner1.jpg');?>
+                            alt="Fourt slide" />
+                        <div class="text-inner-capabilities">
+                            DEVELOPMENTS
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-controler-capabilitie-container">
+                <a href="#carouselControl" role="button" data-slide="next">
+                    <img src=<?php echo base_url('assets/images/next.svg')?> alt="next" />
+                </a>
+                </br>
+                <a href="#carouselControl" role="button" data-slide="prev">
+                    <img src=<?php echo base_url('assets/images/prev.svg')?> alt="next" />
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
 <?php include('footer.php')?>
-    
